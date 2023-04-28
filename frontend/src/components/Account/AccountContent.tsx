573997c89a284945b9acaff32a7bffe171a6c153
Riki Riki <AssasinGeorge1@gmail.com>
2023-04-28 11:35:08 +0300

    Чё та есть

--- frontend/src/components/Account/AccountContent.tsx
+++ frontend/src/components/Account/AccountContent.tsx
@@ -1,14 +1,19 @@
 import { useParams } from "react-router-dom";
 import './AccountContent.css';
 
+import { UserContext} from "../../context/UserContext";
+import { useContext } from "react";
+
 const AccountContent = () => {
     const { UserId } = useParams();
+    const { userPayload } = useContext(UserContext)
 
     return (
-        <div className="Container">
+        <div className="Container" style={{alignItems: "start"}}>
             <div className="AccountContent">
                 <div>
-                    <img src="./AccountLogo.png" alt="" />
+                    <img src="../ImgPerson/1.svg" alt="" />
+                    <h2>{userPayload.username}</h2>
                 </div>
 
                 <div>
